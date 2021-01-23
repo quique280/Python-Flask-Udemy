6149914b0af69cb1ba078b69d9e69d0a48925f52
Enrique Diaz <enrique.diaz@triadhq.com>
2021-01-23 01:50:10 -0600

    JWT extra config

--- section5/code/app.py
+++ section5/code/app.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt import JWT
+
+# from datetime import timedelta
 
 from security import authenticate, identity
 from user import UserRegister
@@ -11,7 +13,22 @@ app = Flask(__name__)
 app.secret_key = "enrique"
 api = Api(app)
 
+# app.config['JWT_AUTH_URL_RULE'] = '/login' # This works if we want to change the default auth endpoint
+# app.config["JWT_EXPIRATION_DELTA"] = timedelta(seconds=1800) # token expiration time
+# app.config["JWT_AUTH_USERNAME_KEY"] = "email"  # config JWT auth key to be email instead of default username
 jwt = JWT(app, authenticate, identity)  # /auth
+
+
+# Authentication response handler
+# @jwt.auth_request_handler
+# def customized_response_handler(access_token, identity):
+#     return jsonify({"access_token": access_token.decode("utf-8"), "user_id": identity.id})
+
+
+# Error handler
+# @jwt.jwt_error_handler
+# def customized_error_handler(error):
+#     return jsonify({"message": error.description, "code": error.status_code}), error.status_code
 
 
 api.add_resource(Item, "/item/<string:name>")  # http://localhost:5000/item/chair
